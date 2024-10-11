32b1db1ff89cc32b7e9558f4720e255ef55b815a
chaitu <chaitus.ch09@gmail.com>
2024-10-11 15:17:04 -0700

    correcting the spelling mistake

--- module/main.tf
+++ module/main.tf
@@ -82,21 +82,21 @@ resource "aws_iam_instance_profile" "instance_profile" {
   role = aws_iam_role.role.name
 }
 
-resource "aws_iam_role" "test_role" {
-  name = "test_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
+#resource "aws_iam_role" "test_role" {
+#  name = "test_role"
+#
+#  assume_role_policy = jsonencode({
+#    Version = "2012-10-17"
+#    Statement = [
+#      {
+#        Action = "sts:AssumeRole"
+#        Effect = "Allow"
+#        Sid    = ""
+#        Principal = {
+#          Service = "ec2.amazonaws.com"
+#        }
+#      },
+#    ]
+#  })
+#}
 
