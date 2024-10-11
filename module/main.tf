8bfa5c7886bbdde0c947587b9a470ca577659673
chaitu <chaitus.ch09@gmail.com>
2024-10-11 14:30:05 -0700

    enabling spot instance

--- module/main.tf
+++ module/main.tf
@@ -10,8 +10,8 @@ resource "aws_instance" "instance" {
   }
   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
-  instance_market_options
-  {
+  instance_market_options {
+    market_type= "spot"
     spot_options {
       instance_interruption_behaviour = "stop"
       spot_instance_type              = "persistent"
