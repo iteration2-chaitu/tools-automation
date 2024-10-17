667b4ee0a6fe567a1833c1b644c5470642ac1fad
chaitu <chaitus.ch09@gmail.com>
2024-10-17 10:23:35 -0700

    error correction

--- module/main.tf
+++ module/main.tf
@@ -63,7 +63,8 @@ resource "aws_iam_role" "role" {
       Version = "2012-10-17"
       Statement = [
         {
-          Action   = concat(var.dummy_policy,var.policy_resource_list)
+          Action   =  var.policy_resource_list
+          //concat(var.dummy_policy,var.policy_resource_list)
          // Action =[["ec2:DescribeInstanceTypes"],var.policy_resource_list]
           //["ec2:Describe*"]
           Effect   = "Allow"
