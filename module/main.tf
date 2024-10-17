4faba8a11db3cf203bd3e4cc6efe870eabe59941
chaitu <chaitus.ch09@gmail.com>
2024-10-17 11:07:58 -0700

    error correction

--- module/main.tf
+++ module/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
-  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+  //iam_instance_profile = aws_iam_instance_profile.instance_profile.name
   //iam_instance_profile = ""
 
   instance_market_options {
@@ -21,8 +21,6 @@ resource "aws_instance" "instance" {
 }
 
 resource "aws_route53_record" "record"{
-  #  ami = ""
-  #  instance_type = ""
   name =  var.tool_name
   type = "A"
   zone_id = var.zone_id
@@ -30,58 +28,56 @@ resource "aws_route53_record" "record"{
   ttl = 30
 }
 
-resource "aws_route53_record" "record-internal"{
-  #  ami = ""
-  #  instance_type = ""
-  name =  "${var.tool_name}-internal"
-  type = "A"
-  zone_id = var.zone_id
-  records = [aws_instance.instance.private_ip]
-  ttl = 30
-}
-
-resource "aws_iam_role" "role" {
-  name = "${var.tool_name}-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-  inline_policy {
-    name = "${var.tool_name}-inline-policy"
-
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action   =  var.policy_resource_list
-          //concat(var.dummy_policy,var.policy_resource_list)
-         // Action =[["ec2:DescribeInstanceTypes"],var.policy_resource_list]
-          //["ec2:Describe*"]
-          Effect   = "Allow"
-          Resource = "*"
-        },
-      ]
-    })
-  }
-
-  tags = {
-    tag-key = "${var.tool_name}-role"
-  }
-}
-
-resource "aws_iam_instance_profile" "instance_profile" {
-  name = "${var.tool_name}-role"
-  role = aws_iam_role.role.name
-}
+#resource "aws_route53_record" "record-internal"{
+#  name =  "${var.tool_name}-internal"
+#  type = "A"
+#  zone_id = var.zone_id
+#  records = [aws_instance.instance.private_ip]
+#  ttl = 30
+#}
+#
+#resource "aws_iam_role" "role" {
+#  name = "${var.tool_name}-role"
+#
+#  assume_role_policy = jsonencode({
+#    Version = "2012-10-17"
+#    Statement = [
+#      {
+#        Action = "sts:AssumeRole"
+#        Effect = "Allow"
+#        Sid    = ""
+#        Principal = {
+#          Service = "ec2.amazonaws.com"
+#        }
+#      },
+#    ]
+#  })
+#  inline_policy {
+#    name = "${var.tool_name}-inline-policy"
+#
+#    policy = jsonencode({
+#      Version = "2012-10-17"
+#      Statement = [
+#        {
+#          Action   =  var.policy_resource_list
+#          //concat(var.dummy_policy,var.policy_resource_list)
+#         // Action =[["ec2:DescribeInstanceTypes"],var.policy_resource_list]
+#          //["ec2:Describe*"]
+#          Effect   = "Allow"
+#          Resource = "*"
+#        },
+#      ]
+#    })
+#  }
+#
+#  tags = {
+#    tag-key = "${var.tool_name}-role"
+#  }
+#}
+#
+#resource "aws_iam_instance_profile" "instance_profile" {
+#  name = "${var.tool_name}-role"
+#  role = aws_iam_role.role.name
+#}
 
 
