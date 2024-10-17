c029f3b4035f1508ba8d5bd03bb7388c00fc14b5
chaitu <chaitus.ch09@gmail.com>
2024-10-17 12:19:07 -0700

    error correction

--- module/main.tf
+++ module/main.tf
@@ -3,16 +3,16 @@ resource "aws_instance" "instance" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
   associate_public_ip_address = true
-  // iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
   //iam_instance_profile = ""
 
-#  instance_market_options {
-#    market_type= "spot"
-#    spot_options {
-#      instance_interruption_behavior = "stop"
-#      spot_instance_type              = "persistent"
-#    }
-#  }
+  instance_market_options {
+    market_type= "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type              = "persistent"
+    }
+  }
 
   tags                   = {
     Name    = var.tool_name
@@ -27,7 +27,7 @@ resource "aws_route53_record" "record"{
   zone_id = var.zone_id
   records = [aws_instance.instance.public_ip]
   ttl = 30
-  depends_on = [aws_instance.instance]
+  //depends_on = [aws_instance.instance]
 }
 
 resource "aws_route53_record" "record-internal"{
@@ -36,51 +36,51 @@ resource "aws_route53_record" "record-internal"{
   zone_id = var.zone_id
   records = [aws_instance.instance.private_ip]
   ttl = 30
-  depends_on = [aws_instance.instance]
+  //depends_on = [aws_instance.instance]
 }
 
-#resource "aws_iam_role" "role" {
-#  name = "${var.tool_name}-role"
-#
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Action = "sts:AssumeRole"
-#        Effect = "Allow"
-#        Sid    = ""
-#        Principal = {
-#          Service = "ec2.amazonaws.com"
-#        }
-#      },
-#    ]
-#  })
-#  inline_policy {
-#    name = "${var.tool_name}-inline-policy"
-#
-#    policy = jsonencode({
-#      Version = "2012-10-17"
-#      Statement = [
-#        {
-#          Action   =  var.policy_resource_list
-#          //concat(var.dummy_policy,var.policy_resource_list)
-#         // Action =[["ec2:DescribeInstanceTypes"],var.policy_resource_list]
-#          //["ec2:Describe*"]
-#          Effect   = "Allow"
-#          Resource = "*"
-#        },
-#      ]
-#    })
-#  }
-#
-#  tags = {
-#    tag-key = "${var.tool_name}-role"
-#  }
-#}
-#
-#resource "aws_iam_instance_profile" "instance_profile" {
-#  name = "${var.tool_name}-role"
-#  role = aws_iam_role.role.name
-#}
-#
+resource "aws_iam_role" "role" {
+  name = "${var.tool_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  inline_policy {
+    name = "${var.tool_name}-inline-policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   =  var.policy_resource_list
+          //concat(var.dummy_policy,var.policy_resource_list)
+         // Action =[["ec2:DescribeInstanceTypes"],var.policy_resource_list]
+          //["ec2:Describe*"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+
+  tags = {
+    tag-key = "${var.tool_name}-role"
+  }
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.tool_name}-role"
+  role = aws_iam_role.role.name
+}
+
 
--- module/variables.tf
+++ module/variables.tf
@@ -1,7 +1,7 @@
 variable "tool_name" {}
 variable "instance_type" {}
 variable "zone_id" {}
-//variable "policy_resource_list" {}
+variable "policy_resource_list" {}
 #variable "dummy_policy" {
 #  default = ["ec2:DescribeInstanceTypes"]
 #}
